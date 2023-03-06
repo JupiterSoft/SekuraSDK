a3243d4a32a0fc4cd7b6e26687ea2d9b43ee0aa7
Akhat T. Kuangaliyev <atak@jupitersoft.kz>
2023-03-06 11:39:01 +0500

    append command line options for load window or widget

--- main.cpp
+++ main.cpp
@@ -3,15 +3,57 @@
  * Author: Akhat T. Kuangaliyev
  * Company: Jupiter Soft
  */
-#include "sdk_mainwindow.h"
+// #include "sdk_mainwindow.h"
 
+#include <MainWindow>
 #include <QApplication>
+#include <QCommandLineParser>
 #include <Sekura>
+#include <appobject.h>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.setApplicationName("SekuraSDK");
+    a.setApplicationVersion("1.0");
+
+    QCommandLineParser parse;
+    parse.setApplicationDescription("SekuraSDK");
+    parse.addHelpOption();
+    parse.addVersionOption();
+    QCommandLineOption conf(QStringList() << "c"
+                                          << "conf",
+                            "set conf <name>", "name");
+    parse.addOption(conf);
+
+    QCommandLineOption startAuth("a", QCoreApplication::translate("main", "Start auth window"));
+    parse.addOption(startAuth);
+    QCommandLineOption startWidget(QStringList() << "w"
+                                                 << "widget",
+                                   "load widget <file_name>", "file_name");
+    parse.addOption(startWidget);
+    parse.process(a);
+
+    QString data_conf = "Default";
+    if (parse.isSet(conf)) {
+        data_conf = parse.value(conf);
+    }
     sekura_init_resources();
-    Sekura::SDK_MainWindow w;
+
+    if (parse.isSet(startAuth)) {
+    } else if (parse.isSet(startWidget)) {
+        Sekura::appObject obj(data_conf);
+        if (obj.isError())
+            return -1;
+        Sekura::BaseWidget *ptr = obj.loadFromFile(parse.value(startWidget));
+        if (ptr != nullptr)
+            obj.startWidget(ptr);
+        return a.exec();
+    }
+
+    Sekura::MainWindow w(data_conf);
+    if (w.isError())
+        return -1;
     w.show();
+
     return a.exec();
 }
